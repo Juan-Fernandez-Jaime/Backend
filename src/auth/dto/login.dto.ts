af130f7ff2b7aee8405e388e2d550355f8fc4e88
ferna <jua.fernandezj@duocuc.cl>
2025-12-01 20:50:56 -0300

    Api Semi funcional 1

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -2,11 +2,18 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString } from 'class-validator';
 
 export class LoginDto {
-  @ApiProperty({ example: 'vendedor@tienda.cl' })
+  // CAMBIO AQUÍ: Ponemos el correo del Admin como ejemplo por defecto
+  @ApiProperty({
+    example: 'admin@tienda.cl',
+    description: 'Correo electrónico del usuario'
+  })
   @IsEmail()
   email: string;
 
-  @ApiProperty({ example: '123' })
+  @ApiProperty({
+    example: '123',
+    description: 'Contraseña del usuario'
+  })
   @IsString()
   password: string;
 }
